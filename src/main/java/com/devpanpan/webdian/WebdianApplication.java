386f0a0bf24a93c9832eb7717363549deb452892
YIHYUN HA <hihyun16@gmail.com>
2022-08-19 13:54:47 +0900

    [Init] Init Project - 스프링 프로젝트 생성 - dependency 설정 - property 설정

---
+++ src/main/java/com/devpanpan/webdian/WebdianApplication.java
@@ -0,0 +1,13 @@
+package com.devpanpan.webdian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpanpan/webdian/WebdianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpanpan.webdian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
